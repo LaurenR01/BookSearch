7b0a7e97376ae4a22c5a6d036ec01e7962f27a69
LaurenR01 <laurenrowe01@gmail.com>
2021-10-08 18:20:38 -0400

    build folder

---
+++ client/build/precache-manifest.30b97c7f92c2bb6a5cd29e6a254358f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f821ad74aae9ac15d77dd4194a942b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aea0a3a974b18bffec38",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "d65d1428cc6ba4906e3d",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "aea0a3a974b18bffec38",
+    "url": "/static/js/2.4e3a88bb.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.4e3a88bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65d1428cc6ba4906e3d",
+    "url": "/static/js/main.92701d7e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
